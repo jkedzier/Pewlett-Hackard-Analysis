82c5c17ce739ff270e35d3ecc61dae704f096410
jkedzier <61914796+jkedzier@users.noreply.github.com>
2020-04-25 15:36:43 -0700

    Add files via upload

--- challenge.sql
+++ challenge.sql
@@ -19,13 +19,43 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 	 AND (t.to_date = '9999-01-01')
 ORDER BY de.dept_no;
 
-
-select title, count(*) as num_retirees from emp_title_info
+--Create a summary count table of the number of retirees by title
+select title, count(*) as num_retirees 
+into title_retiree_count 
+from emp_title_info
 group by title
 order by title;
 
+--Create a list of all employees with their current title
+SELECT e.emp_no,
+	e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+	s.salary
+	INTO emp_title_join
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no) 
+inner join titles as t
+on (e.emp_no=t.emp_no)
+WHERE 
+     (de.to_date = '9999-01-01')
+	 AND (t.to_date = '9999-01-01');
+	 
+--Summarized count of all employees by current title	
+select
+title,
+count(*)
+into total_emp_by_title
+from
+emp_title_join
+group by title;
 
 
+--Query to identify candidates for mentorship
 SELECT e.emp_no,
 	e.first_name,
 e.last_name,
